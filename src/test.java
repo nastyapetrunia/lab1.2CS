a2a74b926b8fdad5b03308d59d03353948e5d929
Nastya <nastyagr217@gmail.com>
2020-10-24 15:17:40 +0300

    edited test class to compare hash codes

--- src/test.java
+++ src/test.java
@@ -7,8 +7,13 @@ public class test {
         Student stud5 = new Student("Jane", 347);
 
         System.out.println("Needs to be true: " + stud1.equals(stud2));
+        System.out.println("Hash stud1: " + stud1.hashCode());
+        System.out.println("Hash stud2: " + stud2.hashCode());
         System.out.println("Needs to be false: " + stud1.equals(stud3));
+        System.out.println("Hash stud3: " + stud3.hashCode());
         System.out.println("Needs to be false: " + stud1.equals(stud4));
+        System.out.println("Hash stud4: " + stud4.hashCode());
         System.out.println("Needs to be false: " + stud1.equals(stud5));
+        System.out.println("Hash stud5: " + stud5.hashCode());
     }
 }
